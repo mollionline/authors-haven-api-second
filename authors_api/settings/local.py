ee5f63557afeffd03cc89a9ee6b8ff6efff63ad4
moldir <auezova.90@mail.ru>
2023-03-07 21:16:21 +0600

    chore: set celery redis flower

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -4,8 +4,13 @@ from .base import env
 
 DEBUG = True
 
-
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='django-insecure-148hvrs@idp!n2aru6o%wv9lqohehp6o2$6jv$ah#xf0&ih1)r')
 
-
 ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default='mailhog')
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "info@authors-haven.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
