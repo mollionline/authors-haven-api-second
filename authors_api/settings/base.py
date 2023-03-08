d97ce4350183dc1130357ff18f04fc69cf390488
moldir <auezova.90@mail.ru>
2023-03-08 14:24:55 +0600

    chore: add profile model and signal set up

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -167,6 +167,11 @@ CELERY_TIMEZONE="Asia/Almaty"
 CELERY_ACCEPT_CONTENT=['json']
 CELERY_TASK_SERIALIZER='json'
 
+REST_FRAMEWORK = {
+    "EXCEPTION_HANDLER": "core_apps.common.exceptions.common_exception_handler",
+    "NON_FIELD_ERRORS_KEY": "error"
+}
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
